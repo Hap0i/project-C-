c40c0606ba7dbba6aa4e84e41a0a84d5f54389d3
Hap0i <vimerpro828@gmail.com>
2025-12-11 00:55:53 +0500

    Update lab5.cpp

--- lab5.cpp
+++ lab5.cpp
@@ -5,20 +5,17 @@
 #include <cstdlib>
 
 int main() {
-    // Установка русской кодировки
     system("chcp 1251");
     setlocale(LC_ALL, "Russian");
     
     // Инициализация генератора случайных чисел
     srand(time(nullptr));
     
-    // Вызов всех заданий
     FileTasks::zadanie1();
     FileTasks::zadanie2();
     FileTasks::zadanie3();
     FileTasks::zadanie4();
     FileTasks::zadanie5();
     FileTasks::zadanie6();
-    
     return 0;
 }
