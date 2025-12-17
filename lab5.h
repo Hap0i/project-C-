2ebd197a2df8187a5f3d056bd1923d591c10242c
Hap0i <vimerpro828@gmail.com>
2025-12-18 02:27:54 +0500

    Update lab5.h

--- lab5.h
+++ lab5.h
@@ -7,12 +7,17 @@
 
 class FileTasks {
 public:
-    // Структура для задания 3
-    struct Baggage {
-        char name[20];
-        double weight = 0;
-    };
-
+    // 1-2
+    static void fillBinaryWithNumbers(const std::string& filename, int count);
+    // 3
+    static void fillBinaryWithBaggage(const std::string& filename, int passengers);
+    // 4
+    static void fillTextOnePerLine(const std::string& filename, int count);
+    // 5
+    static void fillTextMultiplePerLine(const std::string& filename, int lines, int maxPerLine);
+    // 6
+    static void fillTextWithLines(const std::string& filename);
+    
     static void zadanie1();
     static void zadanie2();
     static void zadanie3();
@@ -20,3 +25,9 @@ public:
     static void zadanie5();
     static void zadanie6();
 };
+
+// Структура для задания 3
+    struct Baggage {
+        char name[20];
+        double weight;
+    };
